913b6a0eadab1bdd10385245b4409652a8c9c6ce
Steve Thomson <steve.thomson@pure360.com>
2014-06-25 14:30:52 +0100

    Async hashing/checking, currently a bit buggy

--- app/models/user.js
+++ app/models/user.js
@@ -32,30 +32,23 @@ var userSchema = mongoose.Schema({
   }
 });
 
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+userSchema.methods.validPassword = function(password, done) {
+  bcrypt.compare(password, this.local.password, function(err, valid) {
+    if (err) return done(err);
+    done(null, valid);
+  });
 };
 
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.local.password);
+userSchema.methods.generateHash = function(password, done) {
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return done(err);
+
+    bcrypt.hash(password, salt, undefined, function(err, hash) {
+      if (err) return done(err);
+      done(null, hash);
+    });
+  });
 };
-
-// TODO: change these functions to be async
-// http://devsmash.com/blog/password-authentication-with-mongoose-and-bcrypt
-// bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//     if (err) return next(err);
-
-//     // hash the password along with our new salt
-//     bcrypt.hash(user.password, salt, function(err, hash) {
-//         if (err) return next(err);
-
-//         // override the cleartext password with the hashed one
-//         user.password = hash;
-//         next();
-//     });
-// });
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
